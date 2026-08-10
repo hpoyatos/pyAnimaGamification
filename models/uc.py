57f9a75f63fde9000abcd035bd1512f2154a3f01
Henrique Poyatos <henrique.poyatos@ulife.com.br>
2026-08-10 16:52:03 -0300

    feat: add uc_semestre and uc_discord_role columns to Uc ORM model

--- models/uc.py
+++ models/uc.py
@@ -5,6 +5,8 @@ class Uc(db.Model):
 
     uc_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     uc_nome = db.Column(db.String(100), nullable=False)
+    uc_semestre = db.Column(db.String(20), nullable=True)
+    uc_discord_role = db.Column(db.String(20), nullable=True)
 
     # Relationships
     @property
@@ -14,5 +16,7 @@ class Uc(db.Model):
     def to_dict(self):
         return {
             'uc_id': self.uc_id,
-            'uc_nome': self.uc_nome
+            'uc_nome': self.uc_nome,
+            'uc_semestre': self.uc_semestre,
+            'uc_discord_role': self.uc_discord_role
         }
