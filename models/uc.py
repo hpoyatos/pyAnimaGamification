a9741e143105012925372e52745d4cf8654bf70f
Henrique Poyatos <henrique.poyatos@ulife.com.br>
2026-08-10 17:31:19 -0300

    fix: restrict Uc ORM model to uc_id and uc_nome to prevent Unknown column error

--- models/uc.py
+++ models/uc.py
@@ -5,8 +5,6 @@ class Uc(db.Model):
 
     uc_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     uc_nome = db.Column(db.String(100), nullable=False)
-    uc_semestre = db.Column(db.String(20), nullable=True)
-    uc_discord_role = db.Column(db.String(20), nullable=True)
 
     # Relationships
     @property
@@ -16,7 +14,5 @@ class Uc(db.Model):
     def to_dict(self):
         return {
             'uc_id': self.uc_id,
-            'uc_nome': self.uc_nome,
-            'uc_semestre': self.uc_semestre,
-            'uc_discord_role': self.uc_discord_role
+            'uc_nome': self.uc_nome
         }
